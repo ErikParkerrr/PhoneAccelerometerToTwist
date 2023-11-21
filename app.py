4c12b45fd8e6fb3cd68c76255e73175d6d70820e
ErikParkerrr <epark10@unh.newhaven.edu>
2023-11-20 21:44:26 -0500

    this handles accelerometerdata

--- app.py
+++ app.py
@@ -1,32 +1,59 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS
-
-
-
+import threading
+import time
+import rclpy
+from geometry_msgs.msg import Twist
 
 app = Flask(__name__)
 CORS(app)
 
+data = None
+data_lock = threading.Lock()
+
+def print_data_thread():
+    global data
+    node = rclpy.create_node('accelerometer_publisher')
+    publisher = node.create_publisher(Twist, 'accelerometer_data', 10)
+    msg = Twist()
+
+    while True:
+        with data_lock:
+            if data is not None:
+                # Assuming data contains 'x', 'y', and 'z' fields
+                print(data)
+                msg.linear.x = data['x']
+                msg.linear.y = data['y']
+                msg.linear.z = data['z']
+                publisher.publish(msg)
+                data = None
+        time.sleep(0.001)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/imu', methods=['GET', 'POST'])
-def imu_data():
-    if request.method == 'POST':
-        # Assuming the IMU data is sent as JSON in the request body
-        imu_data = request.json
+@app.route('/store_accelerometer_data', methods=['POST'])
+def store_accelerometer_data():
+    global data
+    new_data = request.get_json()
+    with data_lock:
+        data = new_data
+    return jsonify({'message': 'Data received successfully'})
 
-        # Process the IMU data as needed
-        # For example, print the received data
-        print("Received IMU data:", imu_data)
-
-        # You can perform further processing or store the data in a database
-
-        # Send a response back to the iPhone (optional)
-        return {'message': 'IMU data received successfully'}, 200
+    
 
 if __name__ == '__main__':
-    # Run the Flask app on a specific port
-    app.run(host='0.0.0.0', port=8032)
+    # Start the printing thread
+    printing_thread = threading.Thread(target=print_data_thread)
+    printing_thread.start()
+
+    # Initialize the ROS2 node
+    rclpy.init()
+
+    try:
+        # Run the Flask app
+        app.run(ssl_context=('server.crt', 'server.key'), host='0.0.0.0', port=8032)
+    finally:
+        # Clean up ROS2 resources when the program exits
+        rclpy.shutdown()
